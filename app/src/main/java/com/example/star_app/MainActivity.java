f036ed0e65ffb65c489d90848041f28185607cd2
jed4739 <tjsdlr04@gmail.com>
2022-12-02 16:51:08 +0900

    스플래시 시간을 조정하는 다이얼로그에 정수형만 받게하고 값을 메모리가아닌 디바이스에 저장해야함.

--- app/src/main/java/com/example/star_app/MainActivity.java
+++ app/src/main/java/com/example/star_app/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.star_app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.star_app.databinding.Main;
+
+import java.util.logging.Logger;
+
+/*
+* 구현할 기능 리스트
+*
+* 1. Splash 의 sec(스플래시 초 객체)에 다이어로그 정수값을 입력시킬 방법 찾기
+* 2. 이미지 클릭 시 사진 전체화면
+* 3. 사진전체화면 후에는 뒤로가기 버튼
+* 4. 이미지 줌인 줌 아웃
+* 5. 트와이스까지 총 16명 이미지 생성
+* */
 public class MainActivity extends AppCompatActivity {
-
+    Main binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.dialogButton.setOnClickListener(v -> CustomDialog());
+    }
+    public AlertDialog CustomDialog() {
+        EditText input = new EditText(this);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this)
+                .setTitle("출력할 스플래시 시간을 입력하시오.")
+                .setView(input);
+        dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(input){
+                    Toast.makeText(this,"숫자임",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(this, "숫자만 입력하세요", Toast.LENGTH_SHORT).show();
+                }
+                Log.i("dialog_ok", "Dialog btn ok");
+            }
+        });
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.i("dialog_cancel", "Dialog btn cancel");
+            }
+        });
+        Log.i("dialog", "dialog success");
+        return dialog.show();
+//        try {
+//        } catch (NumberFormatException exception) {
+//        }
     }
 }
--- app/src/main/java/com/example/star_app/Splash.java
+++ app/src/main/java/com/example/star_app/Splash.java
@@ -3,27 +3,29 @@ package com.example.star_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         moveMain(3);
     }
     /*
     * - 문제점
     * 로딩시간에 Splash 가 나올려고 함.
-    * 하지만 Splash 때문에 로딩이 지연됨 -> Splash API 사용
+    * 하지만 Splash 때문에 로딩이 지연됨 -> Splash API 사용 (전부 하고 시간남으면 하기)
     * */
     private void moveMain(int sec) {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                Log.i("splash","splash start");
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+                Log.i("splash", "splash finish");
                 finish();
             }
         },1000 * sec);
