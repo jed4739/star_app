2133e8cfcfb715d32b045236562fdbcb37463670
jed4739 <tjsdlr04@gmail.com>
2022-12-05 15:06:08 +0900

    스플래시 기능 완성 2022.12.05

--- app/src/main/java/com/example/star_app/MainActivity.java
+++ app/src/main/java/com/example/star_app/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
@@ -17,47 +19,56 @@ import java.util.logging.Logger;
 /*
 * 구현할 기능 리스트
 *
-* 1. Splash 의 sec(스플래시 초 객체)에 다이어로그 정수값을 입력시킬 방법 찾기
+* 1. Splash 의 sec(스플래시 초 객체)에 다이어로그 정수값을 입력시킬 방법 찾기 - complete
 * 2. 이미지 클릭 시 사진 전체화면
 * 3. 사진전체화면 후에는 뒤로가기 버튼
 * 4. 이미지 줌인 줌 아웃
 * 5. 트와이스까지 총 16명 이미지 생성
 * */
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
     Main binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences("data", 0);
+        editor = sharedPreferences.edit();
+
         binding.dialogButton.setOnClickListener(v -> CustomDialog());
     }
     public AlertDialog CustomDialog() {
+        Log.i("dialog", "dialog join");
         EditText input = new EditText(this);
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this)
-                .setTitle("출력할 스플래시 시간을 입력하시오.")
-                .setView(input);
-        dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if(input){
-                    Toast.makeText(this,"숫자임",Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(this, "숫자만 입력하세요", Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+            .setTitle("출력할 스플래시 시간을 입력하시오.")
+            .setView(input)
+            .setPositiveButton("Ok", (dialog, which) -> {
+                try {
+                    int input_text = Integer.parseInt(input.getText().toString());
+                    if (input_text > 100) {
+                        Toast.makeText(getApplicationContext(), "스플래시 시간이 너무 길어요!", Toast.LENGTH_SHORT).show();
+                    } else if(input_text <= 0) {
+                        Toast.makeText(getApplicationContext(), "1보다 작을 수 없습니다!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        editor.putInt("input_num", input_text);
+                        editor.commit();
+                        Toast.makeText(getApplicationContext(), "시간 설정 완료!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getApplicationContext(),"숫자만 가능합니다. 다시해주세요!", Toast.LENGTH_LONG).show();
                 }
                 Log.i("dialog_ok", "Dialog btn ok");
-            }
-        });
-        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Log.i("dialog_cancel", "Dialog btn cancel");
-            }
-        });
-        Log.i("dialog", "dialog success");
-        return dialog.show();
-//        try {
-//        } catch (NumberFormatException exception) {
-//        }
+            })
+            .setNegativeButton("Cancel", (dialog, which) -> {
+             dialog.dismiss();
+             Log.i("dialog_cancel", "Dialog btn cancel");
+            });
+
+
+
+        return builder.show();
     }
 }
--- app/src/main/java/com/example/star_app/Splash.java
+++ app/src/main/java/com/example/star_app/Splash.java
@@ -1,33 +1,36 @@
 package com.example.star_app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        moveMain(3);
+        sharedPreferences = getSharedPreferences("data", 0);
+        int input_num = sharedPreferences.getInt("input_num", -1);
+        if (input_num <= 0) {
+            Toast.makeText(getApplicationContext(), "0초 이하는 안됩니다! 기본 3초로 실행합니다.", Toast.LENGTH_SHORT).show();
+            moveMain(3);
+        } else {
+            moveMain(input_num);
+        }
     }
-    /*
-    * - 문제점
-    * 로딩시간에 Splash 가 나올려고 함.
-    * 하지만 Splash 때문에 로딩이 지연됨 -> Splash API 사용 (전부 하고 시간남으면 하기)
-    * */
     private void moveMain(int sec) {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Log.i("splash","splash start");
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                Log.i("splash", "splash finish");
-                finish();
-            }
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Log.i("splash","splash start");
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            Log.i("splash", "splash finish");
+            finish();
         },1000 * sec);
     }
 }
